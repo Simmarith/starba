a444b17d171e81039e95738eb06c9f28d28af7de
Simmarith <menzeladrian@googlemail.com>
2018-08-25 14:29:24 +0200

    Stuck Camera to Player

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+    public GameObject player;
+    public float offsetX;
+    public float offsetY;
+    public float offsetZ;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + new Vector3(offsetX,offsetY,offsetZ);
+        transform.LookAt(player.transform.position);
+	}
+}
